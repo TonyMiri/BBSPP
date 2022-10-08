e632b1365443d89f087f0c68b418045915b769f9
TonyMiri <TonyMiri@users.noreply.github.com>
2022-10-08 18:41:52 -0400

    Blackjack update

--- ascii_blackjack.py
+++ ascii_blackjack.py
@@ -1,7 +1,13 @@
 import random
 
-def draw_card(deck, who, hand, ascii_hand) -> list:
-    return random.choice(deck)
+def draw_card(deck, hand, ascii_hand) -> None:
+    c = random.choice(deck)
+    hand.append(c)
+    all_cards.remove(c)
+    if hand == dealer_hand and first_draw == True:
+        ascii_hand.append(create_card('hide', c))
+    else:
+        ascii_hand.append(create_card('show', c))
 
 def calculate_score(user_cards: list) -> int:
     score = 0
@@ -43,27 +49,32 @@ def create_card(card_type, card: list) -> list:
     line_list.append('|_##|')
     return line_list
 
-def print_cards(cards, multi=False):
-    if multi == False:
-        for x in cards:
-            print(x)
-        return
-
+def print_cards(cards, who, d_hand_showing = False):
     for line in range(4):
         cards_string = ""
         for card in cards:
             cards_string += card[line]
         print(cards_string)
+    if who == 'player':
+        print("_________",
+            f'Your Hand - Current Score: {calculate_score(player_hand)}\n',
+            sep='\n')
+        print(f'Current Bet: {bet_size}')
+        print(f'Remanining Balance: {user_credits}')
+    if who == 'dealer':
+        if d_hand_showing == True:
+            print("_________",
+            f'Dealer Hand - Dealer Score: {calculate_score(dealer_hand)}\n',
+            sep='\n')
+        else:
+            print("_________", 'Dealer Hand', '', sep='\n')
 
-
-
-
-# def get_player_move():
-#     move = input()
+def reveal_dealer_card(dealer_ascii_hand, dealer_hand):
+    dealer_ascii_hand[0] = create_card('show', dealer_hand[0])
 
 if __name__ == '__main__':
     suits = ['D', 'S', 'H', 'C']
-    cards = ['1','2','3','4','5','6','7','8','9','10','J','Q','K','A']
+    cards = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
     all_cards = []
 
     for x in suits:
@@ -74,37 +85,25 @@ if __name__ == '__main__':
     user_credits = 10000
 
     while user_credits > 0:
-    
+        player_hand = []
+        player_ascii_hand = []
+        dealer_hand = []
+        dealer_ascii_hand = []
+        first_draw = True
+        d_hand_showing = False
+
         print(f"Your balance is {user_credits} credits.")
         bet_size = input("How much would you like to bet?\n>")
         user_credits -= int(bet_size)
 
-        player_hand = []
-        player_ascii_hand = []
-        p1 = draw_card(all_cards)
-        all_cards.remove(p1)
-        player_hand.append(p1)
+        draw_card(all_cards, player_hand, player_ascii_hand)
+        draw_card(all_cards, dealer_hand, dealer_ascii_hand)
+        first_draw = False
+        draw_card(all_cards, player_hand, player_ascii_hand)
+        draw_card(all_cards, dealer_hand, dealer_ascii_hand)
 
-        dealer_hand = []
-        dealer_ascii_hand = []
-        d1 = draw_card(all_cards)
-        dealer_hand.append(d1)
-        all_cards.remove(d1)
-        first_draw = True
-
-        for x in player_hand:
-            player_ascii_hand.append(create_card('show', x))
-
-        for x in dealer_hand:
-            if first_draw:
-                dealer_ascii_hand.append(create_card('hide', x))
-                first_draw = False
-                break
-            dealer_ascii_hand.append(create_card('show', x))
-
-        print_cards(dealer_ascii_hand, multi=True)
-        print("_________", 'Dealer Hand', '', sep='\n')
-        print_cards(player_ascii_hand, multi=True)
-        print("_________", 'Your Hand', '', sep='\n')
-        print(f'Current Bet: {bet_size}')
-        print(f'Remanining Balance: {user_credits}')
+        print_cards(dealer_ascii_hand, 'dealer')
+        print_cards(player_ascii_hand, 'player')
+        p_move = input("\nWould you like to (H)it, (S)tand, or (D)ouble?\n>")
+        reveal_dealer_card(dealer_ascii_hand, dealer_hand)
+        print_cards(dealer_ascii_hand, 'dealer', d_hand_showing=True)
