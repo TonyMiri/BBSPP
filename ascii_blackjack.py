fdd79696f5e7327e2cb393cbdcf7950221455903
TonyMiri <TonyMiri@users.noreply.github.com>
2022-10-11 20:00:19 -0400

    Finished Blackjack

--- ascii_blackjack.py
+++ ascii_blackjack.py
@@ -16,7 +16,7 @@ def draw_card(deck, hand, ascii_hand) -> None:
             ascii_hand.append(create_card('show', c))
     else:
         print('Oopsie. We ran out of cards. Reshuffling...')
-        reset_deck(all_cards, reshuffle=True, p=hand, d=dealer_hand)
+        reset_deck(deck, reshuffle=True, p=hand, d=dealer_hand)
         draw_card(deck, hand, ascii_hand)
 #-----
 def calculate_score(user_cards: list) -> int:
@@ -61,26 +61,27 @@ def create_card(card_type, card: list) -> list:
     return line_list
 
 #-----
-def print_cards(cards, d_hand_showing = False):
-    for line in range(4):
-        cards_string = ""
-        for card in cards:
-            cards_string += card[line]
-        print(cards_string)
-    if cards == player_ascii_hand:
-        print("_________",
-            f'Your Hand - Score: {calculate_score(player_hand)}\n',
-            sep='\n')
-        print(f'Current Bet: {bet_size}')
-        print(f'Remanining Balance: {user_credits}')
-        print('------------------------------------------------------------')
-    if cards == dealer_ascii_hand:
-        if d_hand_showing == True:
+def print_cards(cards_list, d_hand_showing = False):
+    for cards in cards_list:
+        for line in range(4):
+            cards_string = ""
+            for card in cards:
+                cards_string += card[line]
+            print(cards_string)
+        if cards == player_ascii_hand:
             print("_________",
-            f'Dealer Hand - Score: {calculate_score(dealer_hand)}\n',
-            sep='\n')
-        else:
-            print("_________", 'Dealer Hand', '', sep='\n')
+                f'Your Hand - Score: {calculate_score(player_hand)}\n',
+                sep='\n')
+            print(f'Current Bet: {bet_size}')
+            print(f'Remanining Balance: {user_credits}')
+            print('------------------------------------------------------------')
+        if cards == dealer_ascii_hand:
+            if d_hand_showing == True:
+                print("_________",
+                f'Dealer Hand - Score: {calculate_score(dealer_hand)}\n',
+                sep='\n')
+            else:
+                print("_________", 'Dealer Hand', '', sep='\n')
 
 #------
 def reveal_dealer_card(dealer_ascii_hand, dealer_hand):
@@ -152,101 +153,93 @@ if __name__ == '__main__':
         draw_card(all_cards, player_hand, player_ascii_hand)
         draw_card(all_cards, dealer_hand, dealer_ascii_hand)
 
-        print_cards(dealer_ascii_hand)
-        print_cards(player_ascii_hand)
+        print_cards([dealer_ascii_hand, player_ascii_hand])
 
         p_score = calculate_score(player_hand)
         d_score = calculate_score(dealer_hand)
         
-        #Check for blackjacks
-        if not (d_score == 21 and p_score == 21):
-            if d_score == 21:
-                
-                reveal_dealer_card(dealer_ascii_hand, dealer_hand)
-                print_cards(dealer_ascii_hand, d_hand_showing=True)
-                print_cards(player_ascii_hand)
-                print('\nUnlucky! Dealer BlackJack :(\n')
-                continue
-            if p_score == 21:
-                user_credits += (math.ceil(bet_size * 2.5))
-                reveal_dealer_card(dealer_ascii_hand, dealer_hand)
-                print_cards(dealer_ascii_hand, d_hand_showing=True)
-                print_cards(player_ascii_hand)
-                print('\nWinner! You got a BlackJack!\n')
-                continue
-        
-        #If no blackjacks...
+        #main action loop
         while calculate_score(player_hand) < 21:
-            p_move = input("\nWould you like to (H)it, (S)tand, or (D)ouble?\n>").lower()
+            if len(player_hand) == 2:
+                p_move = input("\nWould you like to (H)it, (S)tand, or (D)ouble?\n>").lower()
+            else:
+                p_move = input("\nWould you like to (H)it or (S)tand?\n>").lower()
             #Hit
             if p_move in ['h','hit']:
                 draw_card(all_cards, player_hand, player_ascii_hand)
-                print_cards(dealer_ascii_hand)
-                print_cards(player_ascii_hand)
+                print_cards([dealer_ascii_hand, player_ascii_hand])
             #Stand
             elif p_move in ['s', 'stand', 'stay']:
                 print('\nThe dealer shows their cards...')
                 reveal_dealer_card(dealer_ascii_hand, dealer_hand)
-                print_cards(dealer_ascii_hand, d_hand_showing=True)
-                print_cards(player_ascii_hand)
-                if calculate_score(dealer_hand) > calculate_score(player_hand):
-                    break
-                while calculate_score(dealer_hand) < 17:
-                    draw_card(all_cards, dealer_hand, dealer_ascii_hand)
-                    print_cards(dealer_ascii_hand, d_hand_showing=True)
-                    print_cards(player_ascii_hand)
+                print_cards([dealer_ascii_hand, player_ascii_hand],
+                             d_hand_showing=True)
                 break
+                
             #Double Down
             elif p_move in ['d','double','double down','dd','double-down']:
-                print('\nYou double your bet...\n')
-                user_credits -= bet_size
-                bet_size *= 2
-                reveal_dealer_card(dealer_ascii_hand, dealer_hand)
-                draw_card(all_cards, player_hand, player_ascii_hand)
-                print_cards(dealer_ascii_hand, d_hand_showing=True)
-                print_cards(player_ascii_hand)
-                if calculate_score(player_hand) > 21:
+                if len(player_hand) == 2:
+                    print('\nYou double your bet...\n')
+                    user_credits -= bet_size
+                    bet_size *= 2
+                    reveal_dealer_card(dealer_ascii_hand, dealer_hand)
+                    draw_card(all_cards, player_hand, player_ascii_hand)
+                    print_cards([dealer_ascii_hand, player_ascii_hand],
+                                 d_hand_showing=True)
                     break
-                while calculate_score(dealer_hand) < 17:
-                    draw_card(all_cards, dealer_hand, dealer_ascii_hand)
-                    print_cards(dealer_ascii_hand, d_hand_showing=True)
-                    print_cards(player_ascii_hand)
-                break
+                else:
+                    print('You can\'t double down right now. Dummy.')
             else:
                 print('You wanna do what???')
+
         p_final = calculate_score(player_hand)
-        d_final = calculate_score(dealer_hand)
 
         if p_final > 21:
             reveal_dealer_card(dealer_ascii_hand, dealer_hand)
-            print_cards(dealer_ascii_hand, d_hand_showing=True)
-            print_cards(player_ascii_hand)
+            print_cards([dealer_ascii_hand, player_ascii_hand],
+                         d_hand_showing=True)
             print('You busted!')
+            continue
+
+        #dealer action loop
+        while calculate_score(dealer_hand) < 17:
+            if calculate_score(dealer_hand) > calculate_score(player_hand):
+                break
+            else:
+                draw_card(all_cards, dealer_hand, dealer_ascii_hand)
+                print_cards([dealer_ascii_hand, player_ascii_hand],
+                             d_hand_showing=True)
+        
+        d_final = calculate_score(dealer_hand)
 
         if p_final > d_final and p_final <= 21:
             reveal_dealer_card(dealer_ascii_hand, dealer_hand)
-            print_cards(dealer_ascii_hand, d_hand_showing=True)
-            print_cards(player_ascii_hand)
-            print('Congrats! You won!')
-            user_credits += (bet_size * 2)
+            print_cards([dealer_ascii_hand, player_ascii_hand],
+                         d_hand_showing=True)
+            if p_final == 21 and len(player_hand) == 2:
+                print('BlackJack! You win!')
+                user_credits += math.ceil(bet_size * 2.5)
+            else:
+                print('Congrats! You won!')
+                user_credits += (bet_size * 2)
 
         if d_final > p_final and d_final <= 21:
-            print('You lose... Again...')
+            if d_final == 21 and len(dealer_hand) == 2:
+                print('Dealer Blackjack. Unlucky :(')
+            else:
+                print('You lose... Again...')
         
         if d_final > 21:
             print('Dealer Busted! Lucky you ;)')
             user_credits += (bet_size * 2)
 
         if p_final == d_final:
-            print_cards(dealer_ascii_hand, d_hand_showing=True)
-            print_cards(player_ascii_hand)
+            print_cards([dealer_ascii_hand, player_ascii_hand],
+                         d_hand_showing=True)
             print("Pushed. You get your money back.")
             user_credits += bet_size
 
     print("You're outta money, ya bum! Get outta here!")   
 
 #TODO:
-#Correct the behavior for when blackjack is received 
-#Test reshuffling more thoroughly 
 #Refactor print_cards() function?
-#Double option should only be available on first deal
